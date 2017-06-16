43a3a72815b5f31c7b9f83f33e758b1e554622dd
eastwood <eastwood@eastwoods-iMac.local>
2017-06-16 17:54:00 +0500

    Add Class

---
+++ SomeClass.m
@@ -0,0 +1,13 @@
+//
+//  SomeClass.m
+//  Retrowave Player
+//
+//  Created by eastwood on 16/06/2017.
+//  Copyright © 2017 Илья Блинов. All rights reserved.
+//
+
+#import "SomeClass.h"
+
+@implementation SomeClass
+
+@end
